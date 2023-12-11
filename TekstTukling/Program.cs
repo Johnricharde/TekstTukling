3066e201b53999b8d48fb3e841a3e1943f0d47d4
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-12-11 18:49:35 +0100

    Add project files.

---
+++ TekstTukling/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TekstTukling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[1] Rotate Text");
+            Console.WriteLine("[2] Change Text\n");
+            string menuChoice = Console.ReadLine();
+            string userInput;
+
+            switch (menuChoice)
+            {
+                case "1": // Reverses the entire string
+                    Console.WriteLine("Enter the text you want to rotate:");
+
+                    userInput = Console.ReadLine();
+                    char[] chars = userInput.ToCharArray();
+                    Array.Reverse(chars);
+                    Console.WriteLine(new string(chars));
+
+                    Console.ReadKey();
+                    break;
+                case "2": // Replaces every 'e' with an 'a'
+                    Console.WriteLine("Enter the text you want to change:");
+
+                    userInput = Console.ReadLine().ToLower();
+                    Console.WriteLine(userInput.Replace('e', 'a'));
+
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
